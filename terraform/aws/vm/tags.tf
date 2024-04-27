8265263b2861337667c2599c42cc0ff0b014b99d
codecowboydotio <scottvankalken@gmail.com>
2024-04-27 12:02:31 +1000

    updated tags

--- terraform/aws/vm/tags.tf
+++ terraform/aws/vm/tags.tf
@@ -2,9 +2,9 @@ variable "default_ec2_tags" {
   description = "Default set of tags to apply to EC2 instances"
   type        = map
   default = {
-    Owner       = "s.vankalken@f5.com"
+    Owner       = "me"
     Environment = "Demo"
-    SupportTeam = "ANZSE"
-    Contact     = "svk@example.com"
+    SupportTeam = "support_team"
+    Contact     = "me@example.com"
   }
 }
